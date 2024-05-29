12dcc68744ff96aa780efdfc7f9bee53610bca1c
gaozw <private_gzw1314@163.com>
2024-05-29 14:32:38 +0800

    CompletableFuture 爬虫 k8s docker Aigc

--- kd-platform/src/main/java/org/action/thread/ThreadTask.java
+++ kd-platform/src/main/java/org/action/thread/ThreadTask.java
@@ -2,6 +2,8 @@ package org.action.thread;
 
 import java.util.concurrent.ThreadPoolExecutor;
 import javax.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 /**
@@ -14,11 +16,16 @@ import org.springframework.stereotype.Component;
 @Component
 public class ThreadTask {
 
+    public static final Logger LOGGER = LoggerFactory.getLogger(ThreadTask.class);
+
     @Resource
     private ThreadPoolExecutor kdThreadPool;
 
 
     public void send (){
-        kdThreadPool.execute(() -> System.out.println("----------------------------"+Thread.currentThread().getName()));
+        LOGGER.info("CompletableFuture 爬虫 k8s docker Aigc");
+        kdThreadPool.execute(() ->
+                        LOGGER.warn("人生之艰难 就像那不息之长河 虽有东去大海之志 却流程缓慢 征程多坚" +
+                                " 然 江河水总有入海之时，而人生之志 却常常难以实现 令人抱恨终身!!\r\n"));
     }
 }
